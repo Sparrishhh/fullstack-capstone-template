a29414932d9f63917a95c7c373f571bdf457483c
Sparrishhh <sparrish@lelandmanagement.com>
2025-07-30 18:33:12 -0400

    Update frontend config to use working backend URL

--- giftlink-frontend/src/config.js
+++ giftlink-frontend/src/config.js
@@ -1,5 +1,5 @@
 const config = {
-    backendUrl: 'https://sparrish-3060.theiaopenshiftnext-1-labs-prod-theiaopenshift-4-tor01.proxy.cognitiveclass.ai',
+    backendUrl: 'https://sparrish-3060.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai',
   };
   
   console.log(`✅ backendUrl in config.js: ${config.backendUrl}`);
